d4e0ec8cead853737909b917f3ccdf763abb2c4e
duchungpham.dev <phamduchung888888@gmail.com>
2026-02-05 08:39:53 +0700

    update chat IP

--- Student_Management_System/Program.cs
+++ Student_Management_System/Program.cs
@@ -21,11 +21,18 @@ builder.Services.AddDbContext<ChatDbContext>(options =>
 builder.Services.AddScoped<IFileUploadService, FileUploadService>();
 
 // Configure request size for large file uploads (1GB)
+// C?u hình Kestrel ?? listen trên t?t c? các network interfaces (0.0.0.0)
+// Cho phép các máy trong cùng m?ng LAN k?t n?i ???c
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.Limits.MaxRequestBodySize = 1024L * 1024 * 1024; // 1GB per request
     options.Limits.MaxRequestLineSize = 16 * 1024; // 16KB
     options.Limits.MaxRequestHeadersTotalSize = 64 * 1024; // 64KB
+    
+    // Listen trên t?t c? network interfaces v?i c?ng 5000 (HTTP) và 5001 (HTTPS)
+    // Thay localhost b?ng 0.0.0.0 ?? máy khác trong LAN có th? truy c?p
+    options.ListenAnyIP(5000); // HTTP - http://[IP_LAN]:5000
+    options.ListenAnyIP(5001, listenOptions => listenOptions.UseHttps()); // HTTPS
 });
 
 var app = builder.Build();
